4dbd5cc46b7e42b4d65edfc555c1636fb9c890e5
Kreppa1 <161854188+Kreppa1@users.noreply.github.com>
2026-01-21 20:21:53 +0100

    r/place knockoff angefangen dingens

---
+++ Server/src/ClientObject.java
@@ -0,0 +1,19 @@
+import java.net.Socket;
+
+public class ClientObject{
+    public ClientObject(Socket clientSocket, String clientName, int clientChannel, RoleObject clientRole){
+        this.clientSocket=clientSocket;
+        this.clientName=clientName;
+        this.clientChannel=clientChannel;
+        this.clientRole=clientRole;
+    }
+    public Socket clientSocket;
+    public String clientName;
+    public int clientChannel;
+    public RoleObject clientRole;
+
+    public String getDisplayName(){
+        if (clientName!=null) return clientName;
+        else return "?";
+    }
+}
