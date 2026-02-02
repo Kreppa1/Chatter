b8e9503ca48880bc9e06b707edca1b1ef4fca44b
A55adon <simonwa08@gmx.de>
2026-02-02 20:07:07 +0100

    commit❤️

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Chatter Application");
-        System.out.println("Please run Server/Main to start the server.");
-        System.out.println("Please run Client/Main to start a client.");
+        System.out.println("Please run Server/Main to start the server");
+        System.out.println("Please run Client/Main to start a client");
     }
 }
